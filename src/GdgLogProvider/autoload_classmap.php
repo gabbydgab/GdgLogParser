e3aa17d513d8180e2ec646c9c10a55de42895fbf
gabbydgab <gamba@gabbydgab.com>
2013-12-04 21:04:01 -0500

    added autoload_classmap file

---
+++ src/GdgLogProvider/autoload_classmap.php
@@ -0,0 +1,45 @@
+<?php
+
+/*  * 
+ * Copyright (c) 2013, Gab Amba <gamba@gabbydgab.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * * Neither the name of "S.E.A.D.L.E.S.S. Object Oriented Technologies, Inc" 
+ *   nor the names of its contributors may be used to endorse or promote 
+ *   products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace GdgLogProvider;
+
+return [
+    'GdgLogProvider\Module'                            =>  __DIR__ . '/Module.php',
+    'GdgLogProvider\Controller\LogInterface'           =>  __DIR__ . '/src/GdgLogProvider/Controller/LogInterface.php',
+    'GdgLogProvider\Controller\AbstractLogProvider'    =>  __DIR__ . '/src/GdgLogProvider/Controller/AbstractLogProvider.php',
+    
+    'GdgLogProvider\Mapper\LogInterface'    =>  __DIR__ . '/src/GdgLogProvider/Mapper/LogInterface.php',
+    'GdgLogProvider\Mapper\AbstractLogMapper'    =>  __DIR__ . '/src/GdgLogProvider/Mapper/AbstractLogMapper.php',
+    
+    'GdgLogProvider\Entity\LogInterface'    =>  __DIR__ . '/src/GdgLogProvider/Entity/LogInterface.php',
+    'GdgLogProvider\Entity\AbstractLogEntity'    =>  __DIR__ . '/src/GdgLogProvider/Entity/AbstractLogEntity.php',
+    
+];
